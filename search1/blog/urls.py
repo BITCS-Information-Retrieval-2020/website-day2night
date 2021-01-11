0f32e6ea06883370bcc85f3eb693bd85b41a653d
why <1305469680@qq.com>
2021-01-12 01:05:12 +0800

    修改检索结果页实现

---
+++ search1/blog/urls.py
@@ -0,0 +1,28 @@
+"""blogproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf.urls import url, include
+
+app_name = 'blog'
+urlpatterns = [
+    path('', views.index, name='index'),
+    # path('post/<int:pk>/', views.detail, name='detail'),
+    # path('list/<int:page>/', views.show_paperlist, name='list')
+    # url('search/', views.showpaperlist, name='search'),
+    url(r'^results$', views.results, name='results')
+]
